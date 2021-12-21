7e02919e5fd01e38514d2eec3c71d2bb808fa579
Luis Juarez <luisisthejuarez@gmail.com>
2021-12-20 23:38:50 -0500

    Add files via upload

--- Day4/part2.php
+++ Day4/part2.php
@@ -43,7 +43,7 @@ foreach($gameBoards as $num => $bingoBoard) {
 //loop through numbers called
 foreach($numbersCalled as $draw => $numberCalled) {
     //draw a number
-   // echo("Card $draw is $numberCalled <br>");
+    // echo("Card $draw is $numberCalled <br>");
     //check the boards to see which boards have the number
     foreach ($deck as $boardKey => $board) {
         foreach($board as $rowKey => $row) {
@@ -52,51 +52,60 @@ foreach($numbersCalled as $draw => $numberCalled) {
             foreach ($row as $numberKey => $number) {
                 if($number == $numberCalled) {
                     $deck[$boardKey][$rowKey][$numberKey] = "X";
-                   // echo("$numberCalled found on board $boardKey, row $rowKey <br>");
+                    // echo("$numberCalled found on board $boardKey, row $rowKey <br>");
                 }
                 if($deck[$boardKey][$rowKey][$numberKey] != 'X') {
                     $rowWin = false;
                 }
                 $columnWin = true;
                 for($i =0; $i<4;$i++) {
-                   if ($deck[$boardKey][$i][$numberKey] != "X") {
-                       //columnWin
-                       $columnWin = false;
-                   }
+                    if ($deck[$boardKey][$i][$numberKey] != "X") {
+                        //columnWin
+                        $columnWin = false;
+                    }
                 }
             }
             if($columnWin) {
+                $sum = 0;
                 echo("Column win, board: $boardKey <br>");
+
                 //sum of all numbers on the board that were not called
                 foreach($deck[$boardKey] as $check) {
-                    var_dump($check);
+                   //var_dump($check);
                     $sum += array_sum($check);
                 }
-                echo("Sum of unmarked items on the board is $sum");
                 $score = $sum * $numberCalled;
-                var_dump($score);
-                var_dump($draw);
-                var_dump($numberCalled);
-                var_dump($boardKey);
-                exit();
+                unset($deck[$boardKey]);
+                echo("Sum is $sum and number called is $numberCalled");
+//                var_dump($score);
+//                var_dump($draw);
+//                var_dump($numberCalled);
+//                var_dump($boardKey);
+//                exit();
             }
+            $columnWin = false;
             if($rowWin){
+                $sum = 0;
                 //calculate Score
-                echo("Row Win, board: $boardKey");
+                echo("Row Win, board: $boardKey <br>");
+                $rowWin=false;
                 //sum of all numbers on the board that were not called
                 foreach($deck[$boardKey] as $check) {
                     var_dump($check);
                     $sum += array_sum($check);
                 }
-                var_dump($sum);
+                echo("Sum is $sum and number called is $numberCalled");
+              //  var_dump($sum);
                 $score = $sum * $numberCalled;
                 var_dump($score);
-                //
-                var_dump($draw);
-                var_dump($numberCalled);
-                var_dump($boardKey);
-                var_dump($deck[$boardKey]);
-                exit();}
+                unset($deck[$boardKey]);
+//                //
+//                var_dump($draw);
+//                var_dump($numberCalled);
+//                var_dump($boardKey);
+//                var_dump($deck[$boardKey]);
+//                exit();
+            } $rowWin = false;
             //row loop
         }
     }
@@ -107,6 +116,6 @@ foreach($numbersCalled as $draw => $numberCalled) {
 }
 //boards
 
-var_dump($deck); ?>
+ ?>
 
 
