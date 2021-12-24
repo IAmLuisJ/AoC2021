bb36b75163b16571b5c38f04d44064f27d06e1f7
Luis Juarez <luisisthejuarez@gmail.com>
2021-12-23 22:38:40 -0500

    Add files via upload

--- Day5/index.php
+++ Day5/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$inputData = file_get_contents("testdata.txt");
+$inputData = file_get_contents("input.txt");
 
 //parse data
 $arrayData = explode("\n",$inputData);
@@ -14,30 +14,52 @@ foreach($arrayData as $key => $value) {
     $from = trim($line[0]);
     $to = trim($line[1]);
 
+    //get the starting coords
     $xRange = explode(",", $from);
-    $x1 = $xRange[0];
-    $y1 = $xRange[1];
+    $x1 = intval($xRange[0]);
+    $y1 = intVal($xRange[1]);
 
+    //get the ending coords
     $yRange = explode(",", $to);
-    $x2 = $yRange[0];
-    $y2 = $yRange[1];
+    $x2 = intval($yRange[0]);
+    $y2 = intval($yRange[1]);
+
+  //  echo("<br>The line is $x1 , $y1 -> $x2, $y2<br>");
 
     //x1, y1 -> x2 , y2
    echo("<br>The line is $x1 , $y1 -> $x2, $y2");
    //dont assume x2 > x1
-    $distance = abs($x1 - $x2);
-    for($x = $x1; $x <= $x2; $x++) {
-        for($y = $y1; $y <= $y2; $y++) {
-            echo("<br> [$x][$y] ");
-            if($x == $x1 || $y == $y1){
-                echo("<br> [$x][$y] ");
-                $key = json_encode([$x,$y]);
+    $range = range($x1, $x2);
+    foreach($range as $point) {
+       // echo(" <br> point [$point][$y1] ");
+        if($y1 == $y2) {
+                echo(" <br> line [$point][$y1] ");
+                $key = json_encode([$point,$y1]);
                 $cache[$key]++;
-                if($cache[$key] >= 2) {$counter++;echo("overlap");}
+                if($cache[$key] == 2) {$counter++;echo("overlap");}
             }
-        }
-
     }
+    $yrange = range($y1, $y2);
+    foreach ($yrange as $point2) {
+        if($x1 == $x2) {
+            echo(" <br> line [$x1][$point2] ");
+            $key = json_encode([$x1,$point2]);
+            $cache[$key]++;
+            if($cache[$key] == 2) {$counter++;echo("overlap");}
+        }
+        echo(" <br> point [$x1][$point2] ");
+    }
+//    for($x = $x1; $x <= $x2; $x++) {
+//        for($y = $y1; $y <= $y2; $y++) {
+//            echo(" <br> point [$x][$y] ");
+//            if($x1 == $x2 || $y1 == $y2){
+//                echo(" <br> line [$x][$y] ");
+//                $key = json_encode([$x,$y]);
+//                $cache[$key]++;
+//                if($cache[$key] >= 2) {$counter++;echo("overlap");}
+//            }
+//        }
+//    }
 //    var_dump($cache);
 //    exit();
 }
