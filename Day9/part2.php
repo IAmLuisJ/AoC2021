9e4223e04340d156249c60966de0c423ba0fab36
Luis Juarez <luisisthejuarez@gmail.com>
2022-01-04 14:04:51 -0500

    Day 9

--- Day9/part2.php
+++ Day9/part2.php
@@ -1,5 +1,5 @@
 <?php
-$inputData = file_get_contents("input.txt");
+$inputData = file_get_contents("testdata.txt");
 //parse data
 $arrayData = explode("\n",$inputData);
 $counter = 0;
@@ -30,12 +30,29 @@ foreach ($map as $x1 => $row) {
         $below = $map[$x1+1][$y1] ?? 10;
         $left = $map[$x1][$y1-1] ?? 10;
         $right = $map[$x1][$y1+1] ?? 10;
-        if ($point < $above && $point < $below && $point < $left && $point < $right) {
+        if ($point < $above && $point < $below && $point < $left && $point < $right) { //if lowest point
             echo("<br>Low Point at [$x1][$y1] with value of $point");
             $risk = $point + 1;
             array_push($lowPoints, $risk);
+
+            returnBasinNeighbors($x1, $y1);
         }
+
+
     }
+}
+
+//for each low point, find the points in that basin
+function returnBasinNeighbors($x, $y) {
+    static $noNeighbors = false;
+
+    //check adjascent tiles
+    $above = $map[$x-1][$y] ?? 10;
+    $below = $map[$x+1][$y] ?? 10;
+    $left = $map[$x][$y-1] ?? 10;
+    $right = $map[$x][$y+1] ?? 10;
+
+
 }
 
 var_dump(array_sum($lowPoints));
